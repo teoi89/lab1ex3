c8a7666bf48f89115a4f4246fb122fa3114b8bbb
teoi89 <teofil.lucaci@yahoo.ro>
2022-11-28 16:26:58 +0200

    Add project files.

---
+++ lab1ex3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lab1ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
+            tastaura */
+
+            Console.WriteLine("This program will print the digit number of a number");
+            Console.WriteLine("Type the number");
+
+            int yourNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The last digit of your number is " + yourNumber % 10);
+        }
+    }
+}
